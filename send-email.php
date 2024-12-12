e35e5c0916e55630a1296b009ca7a1b9a796de06
clouduser2025 <hussainfarheen915@gmail.com>
2024-12-12 09:42:47 +0530

    Added few portfolio and updated HTML

---
+++ send-email.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form inputs
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Email details
+    $to = "shafeena@adptai.in"; // Your email
+    $subject = "Contact Form Submission from $name";
+    $body = "You have received a new message from your website contact form:\n\n";
+    $body .= "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Message:\n$message\n";
+
+    // Email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message. Please try again later.";
+    }
+}
+?>
